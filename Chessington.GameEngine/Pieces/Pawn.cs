4efe51245cc819784c04bc4131e8af343908ebc3
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2021-11-23 17:22:22 +0000

    Pass Red-17

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -19,25 +19,44 @@ namespace Chessington.GameEngine.Pieces
 
             Square oneAhead;
             Square twoAhead;
+            Square leftDiagonal;
+            Square rightDiagonal;
             
             if (Player == Player.White)
             {
                 oneAhead = Square.At(row - 1, col);
                 twoAhead = Square.At(row - 2, col);
-                
+                leftDiagonal = Square.At(row - 1, col - 1);
+                rightDiagonal = Square.At(row - 1, col + 1);
+
             }
             else
             {
                 oneAhead = Square.At(row + 1, col);
                 twoAhead = Square.At(row + 2, col);
+                leftDiagonal = Square.At(row + 1, col - 1);
+                rightDiagonal = Square.At(row + 1, col + 1);
             }
 
             if (oneAhead.IsInBounds() && board.GetPiece(oneAhead) == null)
             {
                 availableMoves.Add(oneAhead);
-                
+
                 if (!hasMoved && board.GetPiece(twoAhead) == null) availableMoves.Add(twoAhead);
             }
+
+            if (leftDiagonal.IsInBounds() && board.GetPiece(leftDiagonal) != null &&
+                board.GetPiece(leftDiagonal).IsOpponents(Player))
+            {
+                availableMoves.Add(leftDiagonal);
+            }
+            if (rightDiagonal.IsInBounds() && board.GetPiece(rightDiagonal) != null &&
+                board.GetPiece(rightDiagonal).IsOpponents(Player))
+            {
+                availableMoves.Add(rightDiagonal);
+            }
+            
+            
 
             
             return availableMoves;
